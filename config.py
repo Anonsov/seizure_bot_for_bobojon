c89ea1c427b41b548b3c29fd74dab3ae14b21322
Akobir Salomov <akobirsalomov48@gmail.com>
2025-08-24 23:46:02 +0500

    fully done

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-path_to_csv = os.path.join(os.path.dirname(__file__), "data", "seizure_test.csv")
-path_to_medicine_csv = os.path.join(os.path.dirname(__file__), "data", "medicine_test.csv")
+path_to_csv = os.path.join(os.path.dirname(__file__), "data", "seizure.csv")
+path_to_medicine_csv = os.path.join(os.path.dirname(__file__), "data", "medicine.csv")
 admin_list = [
     5460055491, 997175404, 6529721479, 351620312
 ]
